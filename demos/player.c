1523e592aef8810cfca588ef9433bfda737d8483
Tomás Pollak <tomaspollak@gmail.com>
2020-01-18 02:16:20 -0300

    Add demos/player.c

---
+++ demos/player.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include "../src/termbox.h"
+
+tb_color fg_color = TB_WHITE;
+tb_color selected_fg_color = TB_YELLOW;
+tb_color bg_color = TB_DEFAULT;
+
+char * items[] = {
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+  "Option 5",
+  "Option 6",
+  "Option 7",
+  "Option 8",
+  "Option 9",
+  "Option 10",
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+  "Option 5",
+  "Option 6",
+  "Option 7",
+  "Option 8",
+  "Option 9",
+  "Option 10",
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+  "Option 5",
+  "Option 6",
+  "Option 7",
+  "Option 8",
+  "Option 9",
+  "Option 10",
+};
+
+int w = 0, h = 0;
+int selected = -1;
+int offset = 0;
+int num_items = 29;
+
+int margin_left = 1;
+int margin_top = 2;
+int margin_bottom = 2;
+
+int move_offset(int lines) {
+  int menu_h = (h - (margin_top + margin_bottom));
+  int result = lines + offset;
+
+  if (result < 0) {
+    offset = 0;
+    return 0;
+  }
+
+  if (lines > 0 && num_items > 0 && (result > (num_items - menu_h + lines))) {
+    return 0;
+  }
+
+  offset = result;
+  return 0;
+}
+
+/*
+
+int move_selection(int lines) {
+  int menu_h = (h - (margin_top + margin_bottom));
+  int new_sel = selected + lines;
+  if (new_sel > selected && new_sel < offset + menu_h) {
+    // visible
+  }
+}
+*/
+
+
+int move_up(int lines) {
+  selected -= lines;
+  if (selected <= 0) selected = 0;
+  if (selected < offset) offset -= lines;
+  if (offset < 0) offset = 0;
+  return 0;
+}
+
+int move_down(int lines) {
+  int menu_h = (h - (margin_top + margin_bottom));
+
+  selected += lines;
+  if (selected >= num_items) selected = num_items;
+
+  // TODO: simplify this
+  if ((selected + lines) >= menu_h) {
+    if ((offset + lines) <= (num_items - menu_h + lines))
+      offset += lines;
+  }
+
+  return 0;
+}
+
+
+int draw_options(void) {
+  int i, line;
+  char * str;
+  for (i = 0; i < (h - (margin_top + margin_bottom)); i++) {
+    if (i >= num_items) break;
+
+    line = i + offset;
+    str = items[line];
+    if (str)
+      tb_stringf(margin_left, i + margin_top, line == selected ? selected_fg_color : fg_color, bg_color, "%s  ", str);
+    // else
+    //   tb_empty(margin_left, i + margin_top, TB_BLUE, w - margin_left);
+  }
+  return 0;
+}
+
+void draw_title(void) {
+  tb_string(0, 0, fg_color, bg_color, "A music player.");
+}
+
+void draw_status(void) {
+  tb_stringf(0, h-1, fg_color, bg_color, "Selected song: %s", items[selected]);
+}
+
+void draw_window(void) {
+  draw_title();
+  draw_options();
+  draw_status();
+}
+
+int set_selected(int number) {
+  selected = number;
+  return 0;
+}
+
+int play_song(int number) {
+  tb_stringf(w - 15, 0, fg_color, bg_color, "Playing song: %d", number);
+  return 0;
+}
+
+int main(void) {
+  if (tb_init() != 0) {
+    return 1; // couldn't initialize our screen
+  }
+
+  // get the screen resolution
+  w = tb_width();
+  h = tb_height();
+
+  draw_window();
+  tb_render();
+  tb_enable_mouse();
+
+  // now, wait for keyboard or input
+  struct tb_event ev;
+
+  while (tb_poll_event(&ev) != -1) {
+    switch (ev.type) {
+    case TB_EVENT_RESIZE:
+      w = ev.w;
+      h = ev.h;
+      // tb_resize();
+      tb_clear_buffer();
+      tb_stringf((w/2)-10, h/2, fg_color, bg_color, "Window resized to: %dx%d", ev.w, ev.h);
+      break;
+
+    case TB_EVENT_KEY:
+      if (ev.key == TB_KEY_ESC || ev.key == TB_KEY_CTRL_C)
+        goto done;
+
+      if (ev.key == TB_KEY_ENTER)
+        play_song(selected);
+
+      if (ev.key == TB_KEY_ARROW_UP)
+        move_up(1);
+
+      if (ev.key == TB_KEY_ARROW_DOWN)
+        move_down(1);
+
+      if (ev.key == TB_KEY_PAGE_UP)
+        move_up(h / 2);
+
+      if (ev.key == TB_KEY_PAGE_DOWN)
+        move_down(h / 2);
+
+      break;
+    case TB_EVENT_MOUSE:
+      if (ev.key == TB_KEY_MOUSE_LEFT) {
+        set_selected(ev.y - margin_top);
+        // tb_stringf(0, 0, fg_color, bg_color, "Click! Count: %d! (%d, %d)", ev.h, ev.x, ev.y);
+      }
+      break;
+    }
+
+    draw_window();
+    tb_render();
+  }
+
+  done:
+  // make sure to shutdown
+  tb_shutdown();
+  return 0;
+}
